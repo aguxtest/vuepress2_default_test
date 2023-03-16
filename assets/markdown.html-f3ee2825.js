4fadb9d65070e41f238178442701b48b4e6f8d50
aguxtest <aguxgarciabackupsoft_0@protonmail.com>
2023-03-16 15:34:31 -0300

    Updates

---
+++ assets/markdown.html-f3ee2825.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-0978b044","path":"/guide/markdown.html","title":"","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":null,"contributors":[]},"filePathRelative":"guide/markdown.md"}');export{t as data};
